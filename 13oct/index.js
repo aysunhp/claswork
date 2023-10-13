3d81dd8994d799f6e5b05a4c862c38296b91ac9e
aysunhp <145673599+aysunhp@users.noreply.github.com>
2023-10-13 19:48:27 +0400

    obj and arr task added;

--- 13oct/index.js
+++ 13oct/index.js
@@ -1,110 +1,114 @@
-// task1
-// let number = Number(prompt("enter a number"));
-// let word = prompt("enter a word");
-// for (let i = 0; i < number; i++) {
-//   console.log(`${word}`);
+let employees = [
+  {
+    fullname: "Nabil Villanueva",
+    salary: 5000,
+    department: "IT",
+    age: 18,
+    isMarried: false,
+    isMale: true,
+  },
+  {
+    fullname: "Ellis Blanchard",
+    salary: 4000,
+    department: "HR",
+    age: 25,
+    isMarried: true,
+    isMale: false,
+  },
+  {
+    fullname: "Claire Tanner",
+    salary: 800,
+    department: "Finance",
+    age: 23,
+    isMarried: false,
+    isMale: false,
+  },
+  {
+    fullname: "Jaya Casey",
+    salary: 3500,
+    department: "Finance",
+    age: 64,
+    isMarried: true,
+    isMale: true,
+  },
+];
+
+// // task1
+// let sum = 0;
+// for (let i = 0; i < employees.length; i++) {
+//   sum += employees[i].salary;
 // }
+// console.log(sum / employees.length);
 
-// task2
+// // task2
 
-// let num = Number(prompt("enter a number"));
-// let fac = 1;
-
-// for (let i = num; i > 0; i--) {
-//   fac = fac * i;
-// }
-// alert(`${fac}`);
-
-// task3
-
-// let num1 = Number(prompt("enter first number"));
-// let num2 = Number(prompt("enter second number"));
-// if (num1 > num2) {
-//   do {
-//     num2 = Number(prompt("enter second number"));
-//   } while (num2 < num1);
-// }
-// alert(num1 + num2);
-
-// task4
-
-// let num = Number(prompt("enter a number"));
-// let count = 1;
-// let a;
-// while (num > 10) {
-//   a = num % 10;
-//   num = num / 10;
-//   count++;
-// }
-// alert(count);
-
-// task5
-
-// let str = prompt("enter a string");
-// let count = 1;
-// for (let i = 0; i < str.length; i++) {
-//   if (str[i] == ",") {
+// let count = 0;
+// for (let i = 0; i < employees.length; i++) {
+//   if (employees[i].isMale == true) {
 //     count++;
 //   }
 // }
-// alert(count);
+// console.log(count);
 
-// task 6
-
-// let num = Number(prompt("enter a number"));
-// let pow = Number(prompt("enter a number"));
-// let result = 1;
-
-// // alert(num ** pow);
-// for (let i = 1; i <= pow; i++) {
-//   result *= num;
-// }
-
-// alert(result);
-
-// let arr1 = [1, 2, 5, 7, 2, 5, 1];
-// let sum = 0;
-// let avg;
-// for (let i = 0; i <= arr1.length; i++) {
-//   sum += i;
-// }
-// avg = sum / arr1.length;
-// alert(avg);
-
-// let arr1 = [4, 2, 5, 13, 2, 1, 5];
-// let max = arr1[0];
-// let min = arr1[1];
-
-// for (let i = 0; i <= arr1.length; i++) {
-//   if (max < arr1[i]) {
-//     max = arr1[i];
-//   }
-
-//   if (min > arr1[i]) {
-//     min = arr1[i];
+// // task3
+// for (let i = 0; i < employees.length; i++) {
+//   if (employees[i].age > 60) {
+//     let lastTable = employees.splice(i, 1);
 //   }
 // }
-// let avg = (min + max) / 2;
-// alert(avg);
+// console.log(employees);
 
-let arr = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-];
-let count = 0;
-let primeNumbers = [];
+// // task4
+// for (let i = 0; i < employees.length; i++) {
+//   if (employees[i].isMarried == true) {
+//     console.log(employees[i].fullname);
+//   }
+// }
 
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] > 1) {
-    for (let j = 2; j <= Math.sqrt(arr[i]); j++) {
-      if (arr[i] % j === 0) {
-        break;
-      }
-      if (j === Math.sqrt(arr[i])) {
-        count++;
-        primeNumbers.push(arr[i]);
-      }
-    }
-  }
-}
+// // task5
+// for (let i = 0; i < employees.length; i++) {
+//   if (employees[i].salary < 1000) {
+//     employees[i].salary += 500;
+//   }
+// }
+// console.log(employees);
 
-alert(count);
+// // task6
+// let newEmployee = [];
+// for (let i = 0; i < employees.length; i++) {
+//   if (employees[i].department == "IT") {
+//     newEmployee.push(employees[i]);
+//   }
+// }
+// console.log(newEmployee);
+
+// // task7
+// let max = 0;
+// for (let i = 0; i < employees.length; i++) {
+//   if (employees[i].department == "Finance") {
+//     if (employees[i].salary > max) max = employees[i].salary;
+//   }
+// }
+
+// for (let i = 0; i < employees.length; i++) {
+//   if (employees[i].department == "Finance") {
+//     if (employees[i].salary == max) {
+//       console.log(employees[i].fullname);
+//     }
+//   }
+// }
+
+// // task8
+// let min = employees[0].salary;
+// for (let i = 0; i < employees.length; i++) {
+//   if (employees[i].salary < min) {
+//     min = employees[i].salary;
+//   }
+// }
+// for (let i = 0; i < employees.length; i++) {
+//   if (employees[i].salary == min) {
+//     employees[i].salary += 3000;
+//     break;
+//   }
+// }
+// console.log(employees);
